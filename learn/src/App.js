5bec2b440970f4821b840bfaac964f3341564cdf
somesh awasthi <53328679+MrSomeshAwasthi@users.noreply.github.com>
2023-11-25 11:51:05 +0530

    removed file

--- learn/src/App.js
+++
@@ -1,9 +0,0 @@
-import './App.css';
-
-function App() {
-  return (
-    <div>hello from learn app js</div>
-  );
-}
-
-export default App;
--- project/src/App.js
+++
@@ -1,9 +0,0 @@
-import './App.css';
-
-function App() {
-  return (
-    <div>hello from app.js</div>
-  );
-}
-
-export default App;
